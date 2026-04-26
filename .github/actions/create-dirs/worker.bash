b7ae1df74b5f9b055985f91a03c2bb992f3be650
TianYue Ren <godners@163.com>
2026-04-26 16:55:18 +0800

    2026.04.26.20

--- .github/actions/create-dirs/worker.bash
+++ .github/actions/create-dirs/worker.bash
@@ -16,10 +16,12 @@ YEAR=$(echo "$TARGET_DATE" | cut -d'-' -f1)
 MONTH=$(echo "$TARGET_DATE" | cut -d'-' -f2)
 
 BASE_PATH="adjust/$YEAR/$MONTH"
-LOG_PATH="$BASE_PATH/log"
-GRAPH_PATH="$BASE_PATH/graph"
-
-mkdir -p "$LOG_PATH"
-mkdir -p "$GRAPH_PATH"
-
 echo "BASE_PATH=$BASE_PATH" >> "$GITHUB_ENV"
+
+mkdir -p "$BASE_PATH"
+touch "$BASE_PATH/.gitkeep"
+mkdir -p "$BASE_PATH/log"
+touch "$BASE_PATH/log/.gitkeep"
+mkdir -p "$BASE_PATH/graph"
+touch "$BASE_PATH/graph/.gitkeep"
+
