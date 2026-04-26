b8b880f9690c6c83d29c75a3497879f0a36b3573
TianYue Ren <godners@163.com>
2026-04-26 16:47:42 +0800

    2026.04.26.19

--- .github/actions/create-dirs/worker.bash
+++ .github/actions/create-dirs/worker.bash
@@ -15,7 +15,7 @@ fi
 YEAR=$(echo "$TARGET_DATE" | cut -d'-' -f1)
 MONTH=$(echo "$TARGET_DATE" | cut -d'-' -f2)
 
-BASE_PATH=“adjust/$YEAR/$MONTH"
+BASE_PATH="adjust/$YEAR/$MONTH"
 LOG_PATH="$BASE_PATH/log"
 GRAPH_PATH="$BASE_PATH/graph"
 
